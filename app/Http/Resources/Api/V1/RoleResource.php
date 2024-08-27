19b2bfaf63b4f6702b98d6680db0e4872d6f6435
VictorH <vhpm18@gmail.com>
2024-08-27 13:45:07 -0400

    modify import Role and userResource

--- app/Http/Resources/Api/V1/RoleResource.php
+++ app/Http/Resources/Api/V1/RoleResource.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace App\Http\Resources\V1;
 
-use App\Http\Resources\PermissionResource;
+use App\Http\Resources\V1\PermissionResource;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
