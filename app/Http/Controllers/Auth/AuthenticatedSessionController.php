04d0b0fb02e5a26965d3f07c9e5f807ea79b9d8c
VictorH <vhpm18@gmail.com>
2024-08-31 22:00:49 -0400

    adaptacion AuthenticatedSessionController api

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -15,6 +15,7 @@ class AuthenticatedSessionController extends Controller
      */
     public function store(LoginRequest $request): Response
     {
+
         $request->authenticate();
 
         $request->session()->regenerate();
